59eb1c5c975c62e9e6b3d0ebf9579d05c8737308
NathanKaula <kaularaymwadilo@gmail.com>
2022-11-11 18:50:36 +0300

    If Else statements

---
+++ If Else Statements.R
@@ -0,0 +1,41 @@
+#Used to return output based on conditions
+#If (condition) {
+# expression1
+# } else {
+#   expression2
+# }
+count <- 6
+
+if(count >= 5)
+{
+  print("go home")
+} else {
+  print("keep working")
+}
+
+#else if
+if(count >= 5 & count < 10){
+  print("go home")
+} else if(count >= 10) {
+  print("off day")
+} else {
+  "work harder"
+}
+
+
+category <- "D"
+price <- 1000
+
+if(category == "A"){
+  cat("A VAT rate of 8% is applied to this product.", "The total price is
+      :", price * 1.08)
+} else if (category == "B"){
+  cat("A VAT rate of 10% is applied to this product.", "The total price is
+      :", price *1.1)
+} else if (category == "C"){
+  cat("A VAT rate of 20% is applied to this product.", "The total price is
+      :", price * 1.2)
+} else {
+  cat("A VAT rate of 25% is applied to this product.", "The total price is
+      :", price * 1.25)
+}
