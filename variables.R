585184f40e69630a5842a58d23cfa27b1d988f5e
NathanKaula <kaularaymwadilo@gmail.com>
2022-11-11 19:00:02 +0300

    Learning about variables

---
+++ variables.R
@@ -0,0 +1,19 @@
+library(readxl)
+require(readxl)
+df<-readxl::read_xlsx("C:\\Users\\UFAA2\\Documents\\r work\\R training\\WATER 
+                      TANKS INSTALLATION final.xlsx")
+   df2 <- read_excel("WATER TANKS INSTALLATION final.xlsx")
+plot(cars)
+datasets::airmiles
+plot(airmiles)
+list <- c(1,2,3,4,"banana", "orange")
+class(list)
+class(T)
+class(TRUE)
+class(True)
+class(as.logical(Y))
+
+#Maandishi
+total_number_of_tanks_installed <- 5
+totalNumberOfTanksInsulated <- 7
+tota.number.of.tanks.painted = 56
